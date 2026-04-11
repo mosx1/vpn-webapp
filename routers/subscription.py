edde6b8798f5f2b529e1eb9340340fe903b0d339
Кузнецов Максим <m.kuznetsov@aitiguru.com>
2026-04-11 15:09:55 +0300

    fdasf

--- routers/subscription.py
+++ routers/subscription.py
@@ -71,7 +71,7 @@ def _() -> Response:
         }
         subscription_data = base64.b64encode(user.server_link.encode("utf-8")).decode("utf-8")
         if user.protocol == Protocols.amneziawg.value:
-            headers['announce'] = base64.b64encode("The AmnesiaWG protocol is selected in the profile. Download the AmnesiaWG app and set it up.").decode("ascii")
+            headers['announce'] = base64.b64encode("The AmnesiaWG protocol is selected in the profile. Download the AmnesiaWG app and set it up.".encode("utf-8")).decode("ascii")
             subscription_data = "awg"
         return Response(
             subscription_data,
